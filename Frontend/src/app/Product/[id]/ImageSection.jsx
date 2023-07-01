210d18af6ea4bf42187ff345815ba6fabd42acf7
aizaz <i200406@nu.edu.pk>
2023-07-01 18:42:29 +0500

    product page iamges center

--- Frontend/src/app/Product/[id]/ImageSection.jsx
+++ Frontend/src/app/Product/[id]/ImageSection.jsx
@@ -13,10 +13,10 @@ export const ImageSection = ({Images}) => {
 
   return (
     <section className="col-span-1 grid grid-cols-12">
-    <div className=" col-span-12 md:col-span-10">
+    <div className=" col-span-12 lg:col-span-10">
         <img src={mainImage} className="w-full h-full object-cover rounded-xl"></img>
     </div>
-    <div className="md:ml-5  col-span-12 md:col-span-2 flex md:flex-col flex-row flex-wrap  md:flex-nowrap  mt-5 md:mt-0 gap-2 md:mr-5">
+    <div className="lg:ml-5  col-span-12 lg:col-span-2 flex lg:flex-col flex-row flex-wrap justify-center  lg:flex-nowrap  mt-5 lg:mt-0 gap-2 lg:mr-5">
         {
             Images.map((imageUrl,index)=>{
                 return(
